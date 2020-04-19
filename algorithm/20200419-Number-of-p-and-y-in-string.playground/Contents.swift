e415e5712ac1458f2540fd43f056dde64c3c126e
1000JI <chjh1992@gmail.com>
2020-04-19 23:47:37 +0900

    feat: Upload Algorithm Study Contents

---
+++ algorithm/20200419-Number-of-p-and-y-in-string.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+/*
+ 문제 설명
+ 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요. 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+
+ 예를 들어 s가 pPoooyY면 true를 return하고 Pyy라면 false를 return합니다.
+
+ 제한사항
+    - 문자열 s의 길이 : 50 이하의 자연수
+    - 문자열 s는 알파벳으로만 이루어져 있습니다.
+ 
+ 입출력 예
+ s          answer
+ pPoooyY    true
+ Pyy        false
+ */
+
+import Foundation
+
+func solution(_ s:String) -> Bool
+{
+//    var pCount: Int = 0
+//    var yCount: Int = 0
+//
+//    for char in s {
+//        switch char {
+//        case "p", "P":
+//            pCount += 1
+//        case "y", "Y":
+//            yCount += 1
+//        default:
+//            break
+//        }
+//    }
+//    return pCount == yCount ? true : false
+    
+    // OR
+    
+    let string: String = s.lowercased()
+    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count ? true : false
+}
+solution("pPoooyY")
