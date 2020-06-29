24ba39e23beb5e3e783fe562eaaa745cb67f18b0
1000JI <chjh1992@gmail.com>
2020-06-29 16:30:26 +0900

    Challange Practice Example

--- practice/20200629-Practice-DataStructure/LinkedListStack_Single-Linked-List.playground/Contents.swift
+++ practice/20200629-Practice-DataStructure/LinkedListStack_Single-Linked-List.playground/Contents.swift
@@ -81,76 +81,35 @@ linkedList.size
 linkedList.isEmpty
 
 
-//final class SingleLinkedList: LinkedListStack {
-//  private var head: Node? = nil
-//
-//  var isEmpty: Bool { true }
-//
-//  var size: Int = 0
-//
-//  func push(node: Node) {
-//    if head == nil {
-//      head = node
-//      return
-//    }
-//
-//    guard var nodePointer = head else { return }
-//
-//    while nodePointer.next != nil {
-//      nodePointer = nodePointer.next!
-//    }
-//
-//    nodePointer.next = node
-//  }
-//
-//  func peek() -> String? {
-//    guard var nodePointer = head else { return nil }
-//
-//    while nodePointer.next != nil {
-//      nodePointer = nodePointer.next!
-//    }
-//    return nodePointer.value
-//  }
-//
-//  func pop() -> String? {
-//    guard var nodePointer = head else { return nil }
-//    var prevPoint: Node? = nodePointer
-//
-//    while nodePointer.next != nil {
-//      prevPoint = nodePointer
-//      nodePointer = nodePointer.next!
-//    }
-//
-//    let popValue = nodePointer.value
-//    prevPoint?.next = nil
-//
-//    return popValue
-//  }
-//
-//  func printLinked() {
-//    print("-----------")
-//    var headPoint = head
-//    while headPoint != nil {
-//      print("DEBUG: Print \(headPoint?.value)")
-//      headPoint = headPoint?.next
-//    }
-//  }
-//}
-//
-//let stackList: SingleLinkedList = SingleLinkedList()
-//
-//stackList.push(node: Node(value: "A"))
-//stackList.push(node: Node(value: "B"))
-//stackList.push(node: Node(value: "C"))
-//stackList.push(node: Node(value: "D"))
-//stackList.printLinked()
-//
-//print("DEBUG: PeekValue \(stackList.peek())")
-//print("DEBUG: PopValue \(stackList.pop())")
-//print("DEBUG: PopValue \(stackList.pop())")
-//print("DEBUG: PeekValue \(stackList.peek())")
-//stackList.printLinked()
-//
-//stackList.push(node: Node(value: "E"))
-//print("DEBUG: PeekValue \(stackList.peek())")
-//stackList.printLinked()
+/*
+ [ 도전 과제 ]
+ 다음 내용을 참고하여 더블 링크드 리스트 구현
+ 
+ /***************************************************
+ class Node { }
+ - deinit 메서드 구현할 것
+ 
+ class DoubleLinkedList { }
+ - 다음의 프로퍼티와 메서드 구현
+ 
+ [ 프로퍼티 ]
+ // private
+ private var head: Node?
+ private weak var tail: Node?
+ 
+ // internal
+ var isEmpty: Bool - 노드 존재 여부
+ var count: Int    - 노드 전체 개수
+ var first: Node?  - 첫번째 노드 반환
+ var last: Node?   - 마지막 노드 반환
+ 
+ [ 메서드 ]
+ scanValues()  - 현재 저장된 모든 노드의 값 출력
+ removeAll()   - 모든 노드 제거
+ removeNode(by value: String) -> Bool      - 밸류를 이용해 노드 제거 후 성공 여부
+ removeNode(at index: Int) -> String?      - 인덱스를 이용해 노드 제거 후 삭제한 노드의 밸류 반환
+ node(by value: String) -> Node?           - 지정한 값을 지닌 노드를 찾아 반환
+ insert(node newNode: Node, at index: Int) - 특정 위치에 노드 삽입
+ append(node: Node)                        - 마지막 부분에 노드 삽입
+ ***************************************************/
+ */
